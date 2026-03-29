2481a031f244719bbf252232af1e1da2773571ca
Justin <42555501+mountainhokie@users.noreply.github.com>
2026-03-29 12:59:39 -0600

    Media Cards

---
+++ app/playlists/page.tsx
@@ -0,0 +1,9 @@
+export default function PlaylistsPage() {
+  return (
+    <div>
+      <div className="flex items-center justify-between mb-8">
+        <h1 className="text-2xl font-bold text-gray-900">Playlists</h1>
+      </div>
+    </div>
+  );
+}
